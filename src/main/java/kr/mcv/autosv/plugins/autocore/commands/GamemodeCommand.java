954707b3829696375b3d503a47cb3077650a89fd
Black <worldisblack.1@gmail.com>
2021-04-09 22:16:10 +0900

    Added command GamemodeCommand.java (Working)

---
+++ src/main/java/kr/mcv/autosv/plugins/autocore/commands/GamemodeCommand.java
@@ -0,0 +1,16 @@
+package kr.mcv.autosv.plugins.autocore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class GamemodeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        return false;
+    }
+
+}

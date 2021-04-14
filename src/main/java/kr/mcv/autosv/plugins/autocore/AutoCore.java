c63aa1369444fb860d503f0f642c65751407230f
Black <worldisblack.1@gmail.com>
2021-04-14 16:28:50 +0900

    Update AutoCore.java - import static kr.mcv.autosv.plugins.autocore.Utils.Message.*; changed to   import static kr.mcv.autosv.plugins.autocore.Utils.*; - sendMessage() changed to Message.sendMessage()

--- src/main/java/kr/mcv/autosv/plugins/autocore/AutoCore.java
+++ src/main/java/kr/mcv/autosv/plugins/autocore/AutoCore.java
@@ -2,25 +2,27 @@ package kr.mcv.autosv.plugins.autocore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static kr.mcv.autosv.plugins.autocore.Utils.Message.*;
+import static kr.mcv.autosv.plugins.autocore.Utils.*;
 
 public final class AutoCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        sendMessage(console(), L.I, "플러그인을 로드하는 중입니다. Version " + getDescription().getVersion());
+        Message.sendMessage(Message.console(), MLevel.I, "플러그인을 로드하는 중입니다. Version " + getDescription().getVersion());
         long loadTimer = System.currentTimeMillis();
 
         long loadFin = System.currentTimeMillis() - loadTimer;
-        sendMessage(console(), L.I, "플러그인이 로드됬습니다. Time " + loadFin + "ms 소요됨");
+        Message.sendMessage(Message.console(), MLevel.I, "플러그인이 로드됬습니다. Time " + loadFin + "ms 소요됨");
     }
 
     @Override
     public void onDisable() {
-        sendMessage(console(), L.I, "플러그인을 언로드하는 중입니다. Version " + getDescription().getVersion());
+        Message.sendMessage(Message.console(), MLevel.I, "플러그인을 언로드하는 중입니다. Version " + getDescription().getVersion());
         long unloadTimer = System.currentTimeMillis();
 
+        saveDefaultConfig();
+
         long unloadFin = System.currentTimeMillis() - unloadTimer;
-        sendMessage(console(), L.I, "플러그인이 언로드됬습니다. Time " + unloadFin + "ms 소요됨");
+        Message.sendMessage(Message.console(), MLevel.I, "플러그인이 언로드됬습니다. Time " + unloadFin + "ms 소요됨");
     }
 }

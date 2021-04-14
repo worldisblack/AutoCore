20f8dccadb18e5105cfe56a620fe8a0367963567
Black <worldisblack.1@gmail.com>
2021-04-14 16:26:28 +0900

    Update Utils.java - Utils.Message.L is now at Utils.MLevel - Added Level "A". this use at announcement - Making language manage system (Working)

--- src/main/java/kr/mcv/autosv/plugins/autocore/Utils.java
+++ src/main/java/kr/mcv/autosv/plugins/autocore/Utils.java
@@ -10,19 +10,25 @@ public class Utils {
 
         public static String defaultPrefix = "!";
 
-        public static void sendMessage(L level, String message) {
+        public static void sendMessage(MLevel level, String message) {
             sendMessage(defaultPrefix, level, message);
         }
 
-        public static void sendMessage(String prefix, L level, String message) {
+        public static void sendMessage(String prefix, MLevel level, String message) {
             sendMessage(console(), level, message);
         }
 
-        public static void sendMessage(CommandSender sender, L level, String message) {
+        public static void sendMessage(CommandSender sender, MLevel level, String message) {
             sendMessage(sender, defaultPrefix, level, message);
         }
 
-        public static void sendMessage(CommandSender sender, String prefix, L level, String message) {
+        public static void sendMessage(CommandSender sender, String prefix, MLevel level, String message) {
+            if(message.startsWith("@")) {
+                String k = message.substring(0);
+
+            }
+
+            if(message.startsWith("@@")) message = message.substring(0);
             sender.sendMessage(color(level.toString().replace("$p", prefix) + message));
         }
 
@@ -34,26 +40,34 @@ public class Utils {
             return Bukkit.getConsoleSender();
         }
 
-        public enum L {
-            I {
-                @Override
-                public String toString() {
-                    return " &a$p ";
-                }
-            },
-            W {
-                @Override
-                public String toString() {
-                    return " &e$p ";
-                }
-            },
-            E {
-                @Override
-                public String toString() {
-                    return " &c$p ";
-                }
-            }
-        }
 
     }
+
+    public enum MLevel {
+        I { // Info
+            @Override
+            public String toString() {
+                return " &a$p ";
+            }
+        },
+        W { // Warn
+            @Override
+            public String toString() {
+                return " &e$p ";
+            }
+        },
+        E { // Err
+            @Override
+            public String toString() {
+                return " &c$p ";
+            }
+        },
+        A { // Announce
+            @Override
+            public String toString() {
+                return " &6$p ";
+            }
+        }
+    }
+
 }

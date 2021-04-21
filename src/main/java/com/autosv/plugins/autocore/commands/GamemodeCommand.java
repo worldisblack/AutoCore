64a565c66a0c33ceee55bae456746cb0cb3c39d2
worldisblack <worldisblack@gmail.com>
2021-04-21 10:31:26 +0900

    Blocking access from console this will edited

--- src/main/java/com/autosv/plugins/autocore/commands/GamemodeCommand.java
+++ src/main/java/com/autosv/plugins/autocore/commands/GamemodeCommand.java
@@ -1,14 +1,29 @@
-package kr.mcv.autosv.plugins.autocore.commands;
+package com.autosv.plugins.autocore.commands;
 
+
+import com.autosv.plugins.autocore.utils.Utils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Gamemode command for user. overrides vanilla command
+ *
+ * @version 1.0
+ * @since 1.0
+ */
 public class GamemodeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player)) {
+            Utils.Message.sendMessage(sender, Utils.MLevel.I, "");
+            return false;
+        }
+
+
 
         return false;
     }
